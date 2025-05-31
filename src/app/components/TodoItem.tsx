348b27998e2649a46aceac10e14f68a80bd11355
thinhha212 <thinh.haphuc18@gmail.com>
2025-05-31 19:16:40 +0700

    update

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,19 +1,4 @@
 'use client'
 
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { getTodosByKw, getTodo } from "app/lib/api";
-
 export default function TodoItem(){
-
-    const query = useQuery({queryKey: ['todos'], queryFn: () => getTodo()})
-
-    const queryClient = useQueryClient();
-
-    const mutation = useMutation({
-        mutationKey: ['todos'],
-        mutationFn: getTodosByKw,
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['todos']})
-        }
-    })
 }
